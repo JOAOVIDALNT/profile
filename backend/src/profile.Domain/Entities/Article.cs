31c129debfb841ffe3659c9c7e04c796e1e6b3c7
João Vidal <dev.joaovidal@gmail.com>
2025-10-27 23:35:49 -0300

    feat: entities construct and validation

--- backend/src/profile.Domain/Entities/Article.cs
+++ backend/src/profile.Domain/Entities/Article.cs
@@ -4,9 +4,49 @@ namespace profile.Domain.Entities
 {
     public class Article : BaseEntity
     {
-        public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
-        public virtual LocalUser? Author { get; set; }
-        public Guid AuthorId { get; set; }
+        public string Title { get; private set; } = string.Empty;
+        public string Content { get; private set; } = string.Empty;
+        public virtual LocalUser Author { get; private set; }
+        public Guid AuthorId { get; private set; }
+
+        public Article(string title, string content, LocalUser author)
+        {
+            SetTitle(title);
+            SetContent(content);
+            SetAuthor(author);
+        }
+
+        private void SetTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentNullException("title");
+            if (title.Length > 100)
+                throw new ArgumentOutOfRangeException("title");
+            
+            this.Title = title;
+        }
+
+        private void SetContent(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                throw new ArgumentNullException("content");
+
+            if (content.Length < 100)
+                throw new Exception("Content must be at least 100 characters");
+            
+            this.Content = content;
+        }
+
+        private void SetAuthor(LocalUser author)
+        {
+            if (author == null)
+                throw new ArgumentNullException("author");
+
+            if (author.Id == Guid.Empty)
+                throw new ArgumentNullException("author");
+                
+            this.Author = author;
+            this.AuthorId = author.Id;
+        }
     }
 }
--- backend/src/profile.Domain/Entities/Subscriber.cs
+++ backend/src/profile.Domain/Entities/Subscriber.cs
@@ -1,10 +1,39 @@
-﻿using valet.lib.Core.Domain.Entities;
+﻿using System.Net.Mail;
+using valet.lib.Core.Domain.Entities;
 
 namespace profile.Domain.Entities
 {
     public class Subscriber : BaseEntity
     {
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; private set; } = string.Empty;
         public virtual ICollection<LocalUser> Subscriptions { get; set; } = [];
+
+        public Subscriber(string email)
+        {
+            SetEmail(email);
+        }
+
+        private void SetEmail(string email)
+        {
+            if (!IsEmailValid(email))
+                throw new ArgumentException("Invalid email");
+            this.Email = email;
+        }
+        
+        private bool IsEmailValid(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }

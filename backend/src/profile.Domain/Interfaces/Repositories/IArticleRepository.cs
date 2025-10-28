fd429a6a6f3ce353995348a039ed6fd119dd0de8
João Vidal <dev.joaovidal@gmail.com>
2025-10-27 23:02:40 -0300

    feat: repository setup

---
+++ backend/src/profile.Domain/Interfaces/Repositories/IArticleRepository.cs
@@ -0,0 +1,8 @@
+﻿using profile.Domain.Entities;
+using valet.lib.Core.Domain.Interfaces;
+
+namespace profile.Domain.Interfaces.Repositories;
+
+public interface IArticleRepository : IRepository<Article>
+{
+}
